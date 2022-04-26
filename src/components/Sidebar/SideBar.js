a036e943b2c9893bc5108614854caabe4265a8b5
unknown <bryanmej@gmail.com>
2022-04-25 23:35:09 -0400

    added content to sidebar

--- src/components/Sidebar/SideBar.js
+++ src/components/Sidebar/SideBar.js
@@ -1,13 +1,26 @@
 import "./SideBar.scss";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import BLogo from "../../assets/images/b_logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUser, faHome } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = () => (
   <div className="nav-bar">
-    <Link to="/">
+    <Link className="logo" to="/">
       <img src={BLogo} alt="B logo" />
     </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
   </div>
 );
 
