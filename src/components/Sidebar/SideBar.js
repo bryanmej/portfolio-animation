0d6f292d2da91d6d13d77ad3471d9a26534544f8
unknown <bryanmej@gmail.com>
2022-04-27 23:58:39 -0400

    all sidebar elements added

--- src/components/Sidebar/SideBar.js
+++ src/components/Sidebar/SideBar.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import BLogo from "../../assets/images/b_logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faUser, faHome } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = () => (
@@ -11,26 +12,46 @@ const SideBar = () => (
       <img src={BLogo} alt="B logo" />
     </Link>
     <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
+      <NavLink
+        exact="true"
+        className={(navigationData) =>
+          navigationData.isActive ? "active" : null
+        }
+        to="/"
+      >
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
       <NavLink
         exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/"
+        className={(navigationData) =>
+          navigationData.isActive ? "active about-link" : "about-link"
+        }
+        to="/about"
       >
         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
       </NavLink>
       <NavLink
         exact="true"
-        activeclassname="active"
-        className="contact-link"
-        to="/"
+        className={(navigationData) =>
+          navigationData.isActive ? "active contact-link" : "contact-link"
+        }
+        to="/contact"
       >
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a href="https://github.com/bryanmej">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/bryanmej/">
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
