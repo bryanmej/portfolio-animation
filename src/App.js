8eaf4d597f0d41d8120ba3065f03f02b8db6c1fa
bryanmej <bryanmej@gmail.com>
2022-04-29 23:17:00 -0400

    worked on layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/Homepage/HomePage";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Layout />} />
+      <Route path="index" element={<HomePage />} />
     </Routes>
   );
 }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "./Layout.scss";
+import SideBar from "../Sidebar/SideBar";
 
 const Layout = () => (
-  <div>
-    <Outlet />
+  <div className="App">
+    <SideBar />
+    <div className="page">
+      <span className="tags top-tags">&lt;body&gt;</span>
+      <Outlet />
+      <span className="tags bottom-tags">&lt;/body&gt;</span>
+    </div>
   </div>
 );
 
