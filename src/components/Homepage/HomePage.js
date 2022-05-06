a73efc9192894c453a910eab71ecf569fe53b1bf
bryanmej <bryanmej@gmail.com>
2022-05-05 23:44:30 -0400

    implemented text animation styles

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./HomePage.scss";
+import TextAnimation from "../TextAnimation/TextAnimation";
 
-const HomePage = () => (
-  <div className="container home-page">
-    <div className="text-zone">
-      <h1>
-        Hi, <br />
-        I'm Bryan <br />
-        Front-end Developer
-      </h1>
-      <h2>Web developer / Blockchain enthusiast</h2>
-      <Link to="/contact" className="flat-button">
-        Projects
-      </Link>
+const HomePage = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArr = ["B", "r", "y", "a", "n"];
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br />
+          I'm{" "}
+          <TextAnimation
+            letterClass={letterClass}
+            strArray={nameArr}
+            idx={15}
+          />
+          <br />
+          Front-end Developer
+        </h1>
+        <h2>Web developer / Blockchain enthusiast</h2>
+        <Link to="/contact" className="flat-button">
+          Projects
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default HomePage;
--- src/components/TextAnimation/TextAnimation.js
+++ src/components/TextAnimation/TextAnimation.js
@@ -1,11 +1,16 @@
 import "./TextAnimation.scss";
 
-const TextAnimation = ({ letterClass, strArray, idx }) => (
-  <span>
-    {strArray.map((char, i) => (
-      <span></span>
-    ))}
-  </span>
-);
+const TextAnimation = ({ letterClass, strArray, idx }) => {
+  console.log(strArray);
+  return (
+    <span>
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+          {char}
+        </span>
+      ))}
+    </span>
+  );
+};
 
 export default TextAnimation;
