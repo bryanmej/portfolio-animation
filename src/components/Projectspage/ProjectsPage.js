89dba69e29ae1828060cb058908d50aae8039265
bryanmej <bryanmej@gmail.com>
2022-05-21 23:51:31 -0400

    added initial carousel items

--- src/components/Projectspage/ProjectsPage.js
+++ src/components/Projectspage/ProjectsPage.js
@@ -1,7 +1,32 @@
+import { useRef } from "react";
 import "./ProjectsPage.scss";
 
 const ProjectsPage = () => {
-  return <div>Projects</div>;
+  const carouselElem = useRef(null);
+  const cells = window.document.querySelectorAll("carousel__cell");
+
+  console.log(cells);
+  return (
+    <div>
+      <div className="scene">
+        <div ref={carouselElem} className="carousel">
+          <div className="carousel__cell">1</div>
+          <div className="carousel__cell">2</div>
+          <div className="carousel__cell">3</div>
+          <div className="carousel__cell">4</div>
+          <div className="carousel__cell">5</div>
+          <div className="carousel__cell">6</div>
+          <div className="carousel__cell">7</div>
+          <div className="carousel__cell">8</div>
+          <div className="carousel__cell">9</div>
+        </div>
+      </div>
+      <p>
+        <button className="previous-button">Previous</button>
+        <button className="next-button">Next</button>
+      </p>
+    </div>
+  );
 };
 
 export default ProjectsPage;
